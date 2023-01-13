40888cae49785f43307a82d4381bf129d372e635
Junior Alves <jjalves96@gmail.com>
2023-01-13 19:02:51 -0300

    feat: inital config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
+import { Montserrat } from '@next/font/google';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +14,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
-  )
+  );
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+  return (
+    <>
+      <button>asas</button>
+    </>
+  );
+};
